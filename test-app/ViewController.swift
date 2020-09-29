6e9b599ce1e215860cd1968074407fd25210f794
Tommy Myers <tommymyers1101@gmail.com>
2020-09-29 10:22:38 +0100

    Initial Commit

---
+++ test-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test-app
+//
+//  Created by Tommy Myers on 29/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ test-appTests/test_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  test_appTests.swift
+//  test-appTests
+//
+//  Created by Tommy Myers on 29/09/2020.
+//
+
+import XCTest
+@testable import test_app
+
+class test_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
